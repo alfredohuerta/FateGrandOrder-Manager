435c231cf21bfd970d2bdb8088bb44a8b01f5747
Pantro Huerta <pantro12@live.com.mx>
2021-09-18 02:29:44 -0500

    Se estructuro un insertion sort para ordenar de menor a mayor los valores numericos. Se agregó un caso de prueba para comprobar que el algoritmo funciona, se uso un insertion sort por su efectividad para organizar datos ligeramente ordenados porque se busca utilizar para ordenar datos de menor a mayo y viceversa

--- organizacion.h
+++ organizacion.h
@@ -2,7 +2,17 @@
 * Alumno: Erick Alfredo García Huerta
 * Matrícula: A01708119
 * 
-* El programa es capáz de 
+* El programa es capáz de recibir un array de strings y ordenarlos en distintas secuencias.
+*
+* Orden de las clases: 
+* 0 -> Shielder
+* 1 -> Saber
+* 2 -> Lancer
+* 3 -> Archer
+* 4 -> Assasin
+* 5 -> Rider
+* 6 -> Caster
+* 7 -> Berserker
 */
 
 #ifndef ORGANIZACION_H
@@ -11,19 +21,33 @@
 #include <iostream>
 #include <stdio.h>
 #include <vector>
-#include <map>
-#include <fate.csv>
+#include <string>
 
 using namespace std;
 
-
-vector <string> data;
-
-class Organization {
-    public:
-        //something
+template <class T>
+class Organizar{
     private:
-        vector <string> sort();
+        void swap(vector <T> &, int, int);
+    public:
+        vector <T> ordenarPorNumeros(vector <T> &);
 };
+
+template <class T>
+void Organizar<T> :: swap(vector <T> &data, int i, int j){
+    T aux= data[i];
+    data[i]= data[j];
+    data[j]= aux;
+}
+
+template <class T>
+vector <T> Organizar<T> :: ordenarPorNumeros(vector <T> &data){
+    for(int i = 0; i < data.size(); i++){
+        for (int j = i; j >= 0 && data[j] < data[j - 1]; j--) {
+			swap(data, j, j - 1);
+		}
+    }
+    return data;
+}
 
 #endif

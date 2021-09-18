4e3e4b2767acb9c00ce3b5380c658d3c2f1614c5
Pantro Huerta <pantro12@live.com.mx>
2021-09-17 19:11:18 -0500

    se agregó una primera iteración del archivo csv que almacenará los datos a usar

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 /*
 * Alumno: Erick Alfredo García Huerta
 * Matrícula: A01708119
+* 
+* El programa está diseñado para recibir un arreglo de datos y ordenarlos de menor a mayor y viceversa
 */
 
 #include <stdio.h>
 #include <iostream>
+#include <organizacion.h>
 
 using namespace std;
 
---
+++ organizacion.h
@@ -0,0 +1,29 @@
+/*
+* Alumno: Erick Alfredo García Huerta
+* Matrícula: A01708119
+* 
+* El programa es capáz de 
+*/
+
+#ifndef ORGANIZACION_H
+#define ORGANIZACION_H
+
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <map>
+#include <fate.csv>
+
+using namespace std;
+
+
+vector <string> data;
+
+class Organization {
+    public:
+        //something
+    private:
+        vector <string> sort();
+};
+
+#endif
